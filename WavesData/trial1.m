4270ac09f8d490bfaf99a3527e551e77f43fbfef
Raj Garkhedkar <rajgark@gmail.com>
2021-07-20 11:01:17 -0500

    MATLAB reaction diffusion system

---
+++ WavesData/trial1.m
@@ -0,0 +1,36 @@
+t=0:0.05:10;
+d1=0.1; d2=0.1; beta=1.0;
+L=20; n=512; N=n*n;
+x2=linspace(-L/2,L/2,n+1); x=x2(1:n); y=x;
+kx=(2*pi/L)*[0:(n/2-1) -n/2:-1]; ky=kx;
+
+% INITIAL CONDITIONS
+
+[X,Y]=meshgrid(x,y);
+[KX,KY]=meshgrid(kx,ky);
+K2=KX.^2+KY.^2; K22=reshape(K2,N,1);
+
+m=1; % number of spirals
+
+u = zeros(length(x),length(y),length(t));
+v = zeros(length(x),length(y),length(t));
+
+u(:,:,1)=tanh(sqrt(X.^2+Y.^2)).*cos(m*angle(X+i*Y)-(sqrt(X.^2+Y.^2)));
+v(:,:,1)=tanh(sqrt(X.^2+Y.^2)).*sin(m*angle(X+i*Y)-(sqrt(X.^2+Y.^2)));
+
+% REACTION-DIFFUSION
+uvt=[reshape(fft2(u(:,:,1)),1,N) reshape(fft2(v(:,:,1)),1,N)].';
+[t,uvsol]=ode45('ReactionDiffusion',t,uvt,[],K22,d1,d2,beta,n,N);
+
+
+for j=1:length(t)-1
+ut=reshape((uvsol(j,1:N).'),n,n);
+vt=reshape((uvsol(j,(N+1):(2*N)).'),n,n);
+u(:,:,j+1)=real(ifft2(ut));
+v(:,:,j+1)=real(ifft2(vt));
+
+figure(1)
+pcolor(x,y,v(:,:,j+1)); shading interp; colormap(hot); colorbar; drawnow; 
+end
+
+save('/Users/rajgark/Documents/GitHub/COLSA-PySINDy/WavesData/reaction_diffusion_big.m','t','x','y','u','v')
